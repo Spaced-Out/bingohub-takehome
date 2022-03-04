fe9d01d202ff6b1c88e16a68e8ecea147f9a5da8
Marcos Ojeda <subliminal@gmail.com>
2022-03-03 16:28:39 -0800

    update creator to use useNavigate instead of old implicit router prop

--- src/Creator.js
+++ src/Creator.js
@@ -1,55 +1,58 @@
-import React from 'react';
-import {connect} from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import fromPairs from 'lodash/fromPairs';
-import filter from 'lodash/filter';
+import fromPairs from "lodash/fromPairs";
+import filter from "lodash/filter";
 
-import Nav from './Nav';
+import Nav from "./Nav";
 
-import './Creator.css';
+import "./Creator.css";
 
-
-const mapDispatchToProps = dispatch => ({
-  onSubmit(router, event) {
+const mapDispatchToProps = (dispatch) => ({
+  onSubmit(navigate, event) {
     // TODO add game and navigate to its page
   },
 });
 
-const Creator = ({onSubmit, router}) => (
-  <Nav title="New">
-    <form className="creator" onSubmit={event => onSubmit(router, event)}>
-      <div className="left-col">
-        <label>
-          <div>Name</div>
-          <input type="text" name="name" />
-        </label>
+const Creator = ({ onSubmit }) => {
+  const navigate = useNavigate();
 
-        <label>
-          <div>Size</div>
-          <select name="size">
-            <option value="3">3x3</option>
-            <option value="4">4x4</option>
-            <option value="5">5x5</option>
-          </select>
-        </label>
+  return (
+    <Nav title="New">
+      <form className="creator" onSubmit={(event) => onSubmit(navigate, event)}>
+        <div className="left-col">
+          <label>
+            <div>Name</div>
+            <input type="text" name="name" />
+          </label>
 
-        <label>
-          <div>Words</div>
-          <textarea name="words"></textarea>
-        </label>
+          <label>
+            <div>Size</div>
+            <select name="size">
+              <option value="3">3x3</option>
+              <option value="4">4x4</option>
+              <option value="5">5x5</option>
+            </select>
+          </label>
 
-        <button className="creator-submit">Create</button>
-      </div>
+          <label>
+            <div>Words</div>
+            <textarea name="words"></textarea>
+          </label>
 
-      <div className="right-col">
-        <label>
-          <div>Players</div>
-          <input type="number" name="playerCount" />
-        </label>
-      </div>
-    </form>
-  </Nav>
-);
+          <button className="creator-submit">Create</button>
+        </div>
+
+        <div className="right-col">
+          <label>
+            <div>Players</div>
+            <input type="number" name="playerCount" />
+          </label>
+        </div>
+      </form>
+    </Nav>
+  );
+};
 
 export default connect(null, mapDispatchToProps)(Creator);
-
